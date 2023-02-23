ef28a2c0e635481e2493567fd1719288933eb308
jamesbaylor <jamesbay829@gmail.com>
2023-02-24 00:39:54 +0330

    Create getPrime function

--- getPrimes.js
+++ getPrimes.js
@@ -1,5 +1,23 @@
-const getPrimes = () => {
-    return []
+const getPrimes = (n) => {
+    if (n < 1)
+    return [];
+    
+    let primes = [2];
+    let num = 3;
+
+    while (primes.length < n) {
+        let isPrime = true;
+        for (let i=0; i < primes.length  && primes[i] ** 2 <= num; i++) {
+            if (num % primes[i] == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) primes.push(num);
+        num ++;
+    } 
+    
+    return primes;
 }
 
 module.exports = getPrimes;
